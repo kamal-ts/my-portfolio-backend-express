73d4b876f3532f8426eb66ad6099c338e5e7fdac
kamal1234567890 <kamaluddin1623@gmail.com>
2024-05-21 17:37:34 +0900

    update user testing

--- test/user.test.js
+++ test/user.test.js
@@ -29,21 +29,6 @@ describe('POST /api/users', function () {
         expect(result.body.data.password).toBeUndefined;
     });
 
-    it('Soudd able register new user', async () => {
-        const result = await supertest(web)
-            .post('/api/users')
-            .send({
-                username: "kamaluddin",
-                password: "rahasia",
-                name: "La Ode Kamaluddin",
-
-            });
-
-        expect(result.status).toBe(200);
-        expect(result.body.data.username).toBe("kamaluddin");
-        expect(result.body.data.name).toBe("La Ode Kamaluddin");
-        expect(result.body.data.password).toBeUndefined;
-    });
-
+    
 
 })
